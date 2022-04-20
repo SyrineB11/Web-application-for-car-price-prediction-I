a9e8c2dcb61957dd19a77a9d0e1509a627fbc05d
Syrine Bahri <78451998+SyrineB11@users.noreply.github.com>
2022-04-20 20:44:15 +0200

    Update settings.py

--- DzployModel/settings.py
+++ DzployModel/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l5r$0x@tqy-ral%lwyzigmpmqg3e_t00zm=atz0d08$6y%qtmd'
+SECRET_KEY = ########
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
